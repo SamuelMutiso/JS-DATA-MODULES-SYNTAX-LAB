95b7eb4a099e8266fe504d29174380bbea958193
SamuelMutiso <muuosamuel335@gmail.com>
2026-04-15 14:38:20 +0300

    initialized the reuired datejs and combine users function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// we need to import the datejs first and this must be outside the function
+require('datejs')
 
-
+// we now should use the function to combine users and use .....args to collect multiple arrays
+function combineUsers(...args) {
+  // here now we intialize the return object
+  let combinedObject = {
+    users: []
+  };
+  
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
